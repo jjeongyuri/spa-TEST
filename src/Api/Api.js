ebf462dd2e9bb2c81e8c1d2ec4c541bece7106ab
jjeongyuri <jeongyuri1004@gmail.com>
2024-01-17 16:22:41 +0900

    I don't understand(3.)

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -4,16 +4,16 @@ import data from './data.json';
 export function getData(keyword){
     if(!keyword) return data;
     return filterByKeyword(data,keyword)
-};
+}
 
 function filterByKeyword(item,keyword){
     const lowerd = keyword.toLowerCase();
     return item.filter(({titleEnglish})=>titleEnglish.toLowerCase().includes(lowerd))
-};
+}
 
 export function getCoursesBySlug(dataSlug){
     return data.find((data)=>data.slug === dataSlug)
-};
+}
 
 // WISHLIST 필요한것
 const WISHLIST_KEY = 'wishlist';
@@ -25,7 +25,7 @@ export function addWishList(dataSlug){
     // 1. I don't understand
     wishlist[dataSlug] = true;
     localStorage.setItem(WISHLIST_KEY,JSON.stringify(wishlist));
-}
+};
 
 export function getWishList(){
     // 2. I don't understand
@@ -34,6 +34,7 @@ export function getWishList(){
 
 // (Delete)
 export function deleteWishList(dataSlug){
+    // 3. I don't understand
     delete wishlist[dataSlug];
     localStorage.setItem(WISHLIST_KEY,JSON.stringify(wishlist));
 }
